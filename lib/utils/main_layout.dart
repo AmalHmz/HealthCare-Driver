97d1f23047bc075cb07761efc7caa0e73e7637de
AmalHmz <121427490+AmalHmz@users.noreply.github.com>
2023-06-07 14:44:54 +0200

    Add pages
    home and profile

---
+++ lib/utils/main_layout.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:health_care_driver/screens/home_page.dart';
+import 'package:health_care_driver/screens/profile_page.dart';
+
+class MainLayout extends StatefulWidget {
+   const MainLayout({Key? key}) : super(key: key);
+
+  @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  //variable declaration
+  int currentPage = 0;
+  final PageController _page = PageController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _page,
+        onPageChanged: ((value){
+          setState(() {
+            //update page index when tab pressed switch page
+            currentPage = value;
+          });
+        }),
+        children:  <Widget>[
+         
+          HomePage(),
+          ProfilePage(),
+        ],
+      ),
+       
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (page) {
+          setState(() {
+            currentPage = page ;
+            _page.animateToPage(
+            page, 
+            duration: const Duration(milliseconds: 500), 
+            curve: Curves.easeInOut,
+            );
+          });
+        },
+        items: const  <BottomNavigationBarItem>[
+        
+           BottomNavigationBarItem(
+          icon:  FaIcon(FontAwesomeIcons.house),
+          
+          label: 'Home '
+          ),
+          
+          BottomNavigationBarItem(
+          icon: FaIcon(FontAwesomeIcons.user),
+          label: 'Profile'
+          ),
+             
+        ],
+      
+      ),
+    ) ;
+  }
+  
+ 
+  
+  
+}
